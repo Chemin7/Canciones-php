5a35e76e407045b79f9c6133e4c63c25d70dd8dc
Emanuel Vazquez <evazquezrosales@gmail.com>
2023-02-08 15:42:26 -0600

    add files

---
+++ resources/views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Canciones</h1>
+    <ul>
+        <li>Sundress</li>
+        <li>space song</li>
+        <li>Sweet dreams</li>
+    </ul>
+    <?php
+$cars = array("Volvo", "BMW", "Toyota"); 
+
+?>
+</body>
+</html>
